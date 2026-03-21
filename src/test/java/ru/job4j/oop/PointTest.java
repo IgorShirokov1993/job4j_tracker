0dcb03f26dd4a6523970c230ba15581a13260565
IgorShirokov1993 <igorshirokov.1993@gmail.com>
2026-03-21 03:17:20 +0300

    2.1. Рефакторинг - Площадь треугольника. [#122642]

---
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.oop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class PointTest {
+    @Test
+    void when00to20then2() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double distance = a.distance(b);
+        double expected = 2.0;
+        assertThat(distance).isEqualTo(expected);
+    }
+
+    @Test
+    void when13to57then5Dot66() {
+        Point a = new Point(1, 3);
+        Point b = new Point(5, 7);
+        double distance = a.distance(b);
+        double expected = 5.66;
+        assertThat(distance).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenMinus4and0to0Minus3then5() {
+        Point a = new Point(-4, 0);
+        Point b = new Point(0, -3);
+        double distance = a.distance(b);
+        double expected = 5;
+        assertThat(distance).isEqualTo(expected);
+    }
+}
