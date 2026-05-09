e36bf8c995b780ae59a2b17e4341592e8f01ccea
IgorShirokov1993 <igorshirokov.1993@gmail.com>
2026-05-09 10:45:08 +0300

    Тестирование. Подготовка данных.

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -15,4 +15,41 @@ public class StartUITest {
         Item expected = new Item("Fix PC");
         assertThat(created.getName()).isEqualTo(expected.getName());
     }
+
+    /* Порядок действий:
+     * Создаем объект tracker.
+     * Создаем объект item.
+     * Добавляем item в tracker. Получаем id.
+     * Достаем item.id и создаем массив с ответами пользователя.
+     * Вызываем тестируемый метод replaceItem().
+     * Ищем по item.id измененный item в tracker.
+     * Сравниваем имя найденной заявки с ожидаемой.
+     */
+
+    @Test
+    void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()), // id сохраненной заявки в объект tracker.
+                "edited item"
+        };
+        StartUI.replaceItem(new MockInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName()).isEqualTo("edited item");
+    }
+
+    @Test
+    void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+        };
+        StartUI.deleteItem(new MockInput(answers), tracker);
+        Item deleted = tracker.findById(item.getId());
+        assertThat(deleted).isEqualTo(null);
+    }
 }
